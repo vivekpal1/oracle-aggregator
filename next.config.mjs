040c078c4e36da0082198f20b3039ee67c80b7ac
vivek pal <31vivekpal@gmail.com>
2024-07-14 00:35:07 +0000

    fix: nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  target: 'serverless',
   async headers() {
     return [
       {
@@ -11,15 +10,6 @@ const nextConfig = {
         ],
       },
     ];
-  },
-  // Enable verbose logging
-  webpack: (config, { isServer }) => {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.NEXT_VERBOSE': JSON.stringify('true'),
-      })
-    );
-    return config;
   },
 };
 
