f1f081f96df485ab15a8f5f02351a1529cad5547
vivek pal <31vivekpal@gmail.com>
2024-07-14 00:28:04 +0000

    fix: deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    async headers() {
-      return [
-        {
-          source: '/api/:path*',
-          headers: [
-            { key: 'Cache-Control', value: 'no-store, max-age=0' },
-          ],
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
+  reactStrictMode: true,
+  target: 'serverless',
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Cache-Control', value: 'no-store, max-age=0' },
+        ],
+      },
+    ];
+  },
+  // Enable verbose logging
+  webpack: (config, { isServer }) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.NEXT_VERBOSE': JSON.stringify('true'),
+      })
+    );
+    return config;
+  },
+};
+
+export default nextConfig;
